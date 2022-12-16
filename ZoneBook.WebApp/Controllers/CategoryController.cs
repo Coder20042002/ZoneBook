a8bcc5b93b032181ac119bcd5ef7420f4259f381
Coder2042002 <phuonghoai090909@gmail.com>
2022-12-16 21:47:43 +0700

    Add project files.

---
+++ ZoneBook.WebApp/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ZoneBook.WebApp.Controllers.Add;
+using ZoneBook.WebApp.Models;
+
+namespace ZoneBook.WebApp.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public CategoryController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList=_dbContext.Categories;
+            return View(objCategoryList);
+        }
+    }
+}
